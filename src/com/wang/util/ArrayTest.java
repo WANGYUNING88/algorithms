6f96fb4c35054c8d0cf3d11c73233a92aa404329
wangyuning88 <19970305.wang>
2020-05-14 10:29:19 +0800

    java面试题

--- src/com/wang/util/ArrayTest.java
+++ src/com/wang/util/ArrayTest.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        String s = "wang";
-        StringBuilder sb1 = new StringBuilder("wang");
-        StringBuffer sb2 = new StringBuffer("wang");
-//        System.out.println(sb1.);
+//        String s = "wang";
+//        StringBuilder sb1 = new StringBuilder("wang");
+//        StringBuffer sb2 = new StringBuffer("wang");
+////        System.out.println(sb1.);
+//
+//        System.out.println(sb2.reverse());
+//        List<String> list = new ArrayList<>();
+////        list.sort();
 
-        System.out.println(sb2.reverse());
-        List<String> list = new ArrayList<>();
-//        list.sort();
+        Integer f1 = 100, f2 = 100, f3 = 150, f4 = 150;
+        System.out.println(f1 == f2);
+        System.out.println(f3 == f4);
     }
 }
